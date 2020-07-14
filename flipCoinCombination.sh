28e0c8229bb809938fb54f7c1705f9d7d6e2ab11
Akshita-Veerannagari <akshitareddy.v@gmail.com>
2020-07-14 09:24:57 +0530

    Simulating doublet combinations along with percentage

--- flipCoinCombination.sh
+++ flipCoinCombination.sh
@@ -1,16 +1,26 @@
 #! /bin/bash
 
 declare -A coinFlips
-coinFlips["heads"]=0
-coinFlips["tails"]=0
+coinFlips["HH"]=0
+coinFlips["HT"]=0
+coinFlips["TH"]=0
+coinFlips["TT"]=0
+
 for(( counter=1; counter<=10; counter++ ))
 do
-	num=$(( RANDOM ))
-	if [ $(( num % 2)) -eq 0 ]
+	num1=$(( RANDOM ))
+	num2=$(( RANDOM ))
+	if [ $(( num1 % 2)) -eq 0  -a $(( num2%2 )) -eq 0 ]
 	then
-	        coinFlips["heads"]=$(( ${coinFlips[heads]}+1 ))
+	        coinFlips["HH"]=$(( ${coinFlips[HH]}+1 ))
+	elif [ $(( num1%2 )) -eq 0 -a $(( num2%2 )) -eq 1 ]
+	then
+		coinFlips["HT"]=$(( ${coinFlips[HT]}+1 ))
+	elif [ $(( num1%2 )) -eq 1 -a $(( num2%2 )) -eq 0 ]
+	then
+		coinFlips["TH"]=$(( ${coinFlips[TH]}+1 ))
 	else
-	        coinFlips["tails"]=$(( ${coinFlips[tails]}+1 ))
+	        coinFlips["TT"]=$(( ${coinFlips[TT]}+1 ))
 	fi
 done
 
@@ -19,8 +29,12 @@ do
 	echo "$index: ${coinFlips[$index]}"
 done
 
-percentage_head=`echo ${coinFlips["heads"]} 10 100 | awk '{print ($1/$2)*$3}'`
-percentage_tail=`echo ${coinFlips["tails"]} 10 100 | awk '{print ($1/$2)*$3}'`
+percentage_HH=`echo ${coinFlips["HH"]} 10 100 | awk '{print ($1/$2)*$3}'`
+percentage_TT=`echo ${coinFlips["TT"]} 10 100 | awk '{print ($1/$2)*$3}'`
+percentage_HT=`echo ${coinFlips["HT"]} 10 100 | awk '{print ($1/$2)*$3}'`
+percentage_TH=`echo ${coinFlips["TH"]} 10 100 | awk '{print ($1/$2)*$3}'`
 
-echo "Percentage of occurence of heads is $percentage_head"
-echo "Percentage of occurence of tails is $percentage_tail"
+echo "Percentage of occurence of HH is $percentage_HH"
+echo "Percentage of occurence of HT is $percentage_HT"
+echo "Percentage of occurence of TH is $percentage_TH"
+echo "Percentage of occurence of TT is $percentage_TT"
